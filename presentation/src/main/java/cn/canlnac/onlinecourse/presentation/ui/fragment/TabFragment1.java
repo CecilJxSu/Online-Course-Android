9dd622a382253adfaf7fd72b2036365b6ccf8742
CecilJxSu <957005719@qq.com>
2016-11-03 18:03:29 +0800

    Add carousel

--- presentation/src/main/java/cn/canlnac/onlinecourse/presentation/ui/fragment/TabFragment1.java
+++ presentation/src/main/java/cn/canlnac/onlinecourse/presentation/ui/fragment/TabFragment1.java
@@ -6,7 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.canlnac.onlinecourse.presentation.R;
 
 /**
@@ -14,9 +20,32 @@ import cn.canlnac.onlinecourse.presentation.R;
  */
 
 public class TabFragment1 extends Fragment {
+    @BindView(R.id.carouselView)
+    CarouselView carouselView;
+
+    int[] carousalImages = {
+            R.drawable.carousel_image_1,
+            R.drawable.carousel_image_2,
+            R.drawable.carousel_image_3,
+            R.drawable.carousel_image_4,
+            R.drawable.carousel_image_5
+    };
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+        View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+        ButterKnife.bind(this,view);
+
+        carouselView.setPageCount(carousalImages.length);
+
+        carouselView.setImageListener(imageListener);
+        return view;
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(carousalImages[position]);
+        }
+    };
 }
