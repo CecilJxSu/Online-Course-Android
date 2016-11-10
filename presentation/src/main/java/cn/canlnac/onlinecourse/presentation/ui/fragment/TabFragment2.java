810970593ce4a578a5fbc68dfab8ddbeb1978030
ceciljxsu <ceciljxsu@gmail.com>
2016-11-10 23:26:31 +0800

    Add floating action button

--- presentation/src/main/java/cn/canlnac/onlinecourse/presentation/ui/fragment/TabFragment2.java
+++ presentation/src/main/java/cn/canlnac/onlinecourse/presentation/ui/fragment/TabFragment2.java
@@ -6,17 +6,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.canlnac.onlinecourse.presentation.R;
 
 /**
- * Created by cecil on 2016/10/31.
+ * 话题.
  */
 
 public class TabFragment2 extends Fragment {
+    @BindView(R.id.menu_red)
+    FloatingActionMenu menu;
+
+    @BindView(R.id.create_chat)
+    FloatingActionButton createChatButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+        //获取布局
+        View view = inflater.inflate(R.layout.tab_fragment_2, container, false);
+
+        //绑定视图
+        ButterKnife.bind(this, view);
+
+        createChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(TabFragment2.this.getActivity(), "点击事件，创建课程", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
